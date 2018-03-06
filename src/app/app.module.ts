8a4c5720319bf6e1665d0a34676d1b9aa2c3fd9e
Svetlin Kozhuharov <kozhuhar@progress.com>
2018-03-06 15:23:41 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,79 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Http, HttpModule } from '@angular/http';
+// vendor dependencies
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// app
+import { Config } from './common/index';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
-import { environment } from '../environments/environment';
+// Kendo UI
+import { GridModule } from '@progress/kendo-angular-grid';
+import { ChartsModule } from '@progress/kendo-angular-charts';
+import { DialogModule } from '@progress/kendo-angular-dialog';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+
+// Components
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { SigninComponent } from './signin/signin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { IssuesComponent } from './issues/issues.component';
+import { LabelClass } from './issues/label.directive';
+import { MarkdownComponent } from './markdown/markdown.component';
+import { ActiveIssuesComponent } from './charts/active-issues.component';
+import { TypesDistributionComponent } from './charts/types-distribution.component';
+import { IssueTypesComponent } from './charts/issue-types.component';
+import { StatisticsComponent } from './charts/statistics.component';
+
+
+
+Config.PLATFORM_TARGET = Config.PLATFORMS.WEB;
+
+export function createTranslateLoader(http: Http) {
+    return new TranslateHttpLoader(<any>http, './assets/i18n/', '.json');
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        LabelClass,
+        AppComponent,
+        MainMenuComponent,
+        SigninComponent,
+        DashboardComponent,
+        ProfileComponent,
+        IssuesComponent,
+        MarkdownComponent,
+        ActiveIssuesComponent,
+        TypesDistributionComponent,
+        IssueTypesComponent,
+        StatisticsComponent
+    ],
+    imports: [
+        AppRoutingModule,
+        BrowserModule,
+        ChartsModule,
+        GridModule,
+        DialogModule,
+        InputsModule,
+        ButtonsModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        HttpModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (createTranslateLoader),
+                deps: [Http]
+            }
+        })
+    ],
+    providers: [],
+    bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
